9060d5df7c59c6502953ca74a1e650d077a8b557
tsvetaevAlex <you@example.com>
2022-06-02 01:43:23 +0300

    update SqlWrapper. worable create DB create TAble

--- SqlWrapper.py
+++ SqlWrapper.py
@@ -20,21 +20,21 @@ class SqlWrapper:
     def create_table_ships(self):
         print("we are in create_table_ship")
         self.cursor.execute("create table Ships(ship text NOT NULL PRIMARY KEY,weapon text[not NULL], hull text[NOT "
-                            "NULL]engine text[not NULL]);")
+                            + "NULL]engine text[not NULL]);")
 
     def create_table_hull(self):
         print("we are in create_table_hull")
-        self.cursor.execute("'create table Hull(ship text NOT NULL PRIMARY KEY,hull text [not NULL], armor int [NOT "
-                            "NULL]ArmorType int[not NULL]capacity int[not NULL]);")
+        self.cursor.execute("create table Hull(ship text NOT NULL PRIMARY KEY,hull text [not NULL], armor int [NOT "
+                            + "NULL]ArmorType int[not NULL]capacity int[not NULL]);")
 
     def create_table_weapons(self):
         print("we are create_table_weapon")
-        self.cursor.execute("create  table Weapons(weapon text NOT NULL Primary KEY,reloadSpeed int[not NULL] "
-                            "range text[not NULL], powerVolley text[not NULL]count text[not NULL]);")
+        self.cursor.execute("create  table Weapons(weapon text NOT NULL Primary KEY,reloadSpeed int[not NULL] range "
+                            + "text[not NULL], powerVolley text[not NULL]count text[not NULL]);")
 
     def create_table_engines(self):
         print("we are in create_table_engine")
         self.cursor.execute("create table Engine(engine text NOT NULL PRIMARY KEY,EnginePower int [not NULL], "
-                            "EngineType int [not NULL]);")
+                            + "EngineType int [not NULL]);")
 
         self.dbConnection.commit()
