ecc4d86b6126011cce7bbc51d6f5ee6d49c5da0b
tsvetaev.alex <tsvetaev.alex@gmail.com>
2023-12-01 16:08:08 +0300

    Update LinkedList.py
    add reverse print of linked list of elements in range(1, 5)

--- LinkedList.py
+++ LinkedList.py
@@ -1,47 +1,45 @@
 class Elem:
-
-    def __init__(self, d, n = None):
+    def __init__(self, d, n=None):
         self.data = d
         self.next = n
 
-    def __str__(self):
-        return str(self.data)
-
-
 def init_list(arg):
-    list_lenght = len(arg)
-    print(f"inbound value: {arg}")
-    print(f"arg lenght : {list_lenght}")
-    previous = Elem(arg[0], None)  # 1st Root element of linked list
-    root_node = previous
-    i = 1  # continue creation from 'index' = 2 because root node with number 1 is all ready created
-    while i <= list_lenght:
-        current = Elem(i, None)
-        print(f'new node created with Serial number: {i}"')
+    list_length = len(arg)
+    print(f"Inbound value: {arg}")
+    print(f"Argument length: {list_length}")
+
+    root_node = Elem(arg[0], None)  # 1st Root element of linked list
+    print(f'root node created with number: {root_node.data}')
+    previous = root_node
+
+    i = 1  # continue creation from 'index' = 2 because the root node with number 1 is already created
+    while i < list_length:
+        current = Elem(arg[i], None)
+        print(f'New node created with Serial number: {arg[i]}')
         previous.next = current
         previous = current
         i += 1
-    print(f"linked list initialization done.")
+
+    print("Linked list initialization done.")
     return root_node
 
-
-def print_list(node: Elem):
-    while (node is not None):
-        print(node.data)
-        node = node.next
-
-
-def main(self):
-    root = init_list(range(1, 5))
-    print("---")
-  #  reversed = reverse_list(root)
-  #  print_list(reversed)
-
+def print_list(node, reverse=False):
+    if not reverse:
+        while node is not None:
+            print(node.data)
+            node = node.next
+    else:
+        if node is not None:
+            print_list(node.next, reverse=True)
+            print(node.data)
 
 def main():
     root = init_list(range(1, 5))
+    print("Original Linked List:")
     print_list(root)
     print("---")
+    print("Reversed Linked List:")
+    print_list(root, reverse=True)
 
 if __name__ == '__main__':
     main()
